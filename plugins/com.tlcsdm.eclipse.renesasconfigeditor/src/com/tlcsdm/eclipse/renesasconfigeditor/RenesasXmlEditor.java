aafca64b1cd544243d0de9510bf524258ed06c70
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2025-07-07 21:33:25 +0800

    feat: Improve editor

---
+++ plugins/com.tlcsdm.eclipse.renesasconfigeditor/src/com/tlcsdm/eclipse/renesasconfigeditor/RenesasXmlEditor.java
@@ -0,0 +1,8 @@
+package com.tlcsdm.eclipse.renesasconfigeditor;
+
+import org.eclipse.wst.xml.ui.internal.tabletree.XMLMultiPageEditorPart;
+
+@SuppressWarnings("restriction")
+public class RenesasXmlEditor extends XMLMultiPageEditorPart {
+
+}
