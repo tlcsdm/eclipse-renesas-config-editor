bc2adf3739dfcde77a75a428970a51a676f59c2e
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2025-07-08 10:22:02 +0800

    feat: Support inf, html, h, c format
    Close #6

---
+++ plugins/com.tlcsdm.eclipse.renesasconfigeditor/src/com/tlcsdm/eclipse/renesasconfigeditor/RenesasGenericEditor.java
@@ -0,0 +1,8 @@
+package com.tlcsdm.eclipse.renesasconfigeditor;
+
+import org.eclipse.ui.internal.genericeditor.ExtensionBasedTextEditor;
+
+@SuppressWarnings("restriction")
+public class RenesasGenericEditor extends ExtensionBasedTextEditor {
+
+}
